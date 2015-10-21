eac09e34377ad96d2a66a5921a945d8bd72e793a
hsusmita <susmita.horrow@gmail.com>
2015-10-21 07:25:44 +0530

    backbutton configuration

--- ICSAdmin/ICSAdmin/Controllers/CustomNavigationController.m
+++ ICSAdmin/ICSAdmin/Controllers/CustomNavigationController.m
@@ -20,55 +20,14 @@
 
 - (void)navigationController:(UINavigationController *)navigationController
 	  willShowViewController:(UIViewController *)viewController animated:(BOOL)animated {
-//  [self configureMenuButton];
   NSMutableArray *leftBtns = [[NSMutableArray alloc] init];
   
   UIBarButtonItem *leftBtn = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"menuButton"] style:UIBarButtonItemStylePlain target:self action:@selector(leftButtonPressed)];
   [leftBtns addObject:leftBtn];
-  
-  NSLog(@"nav item = %@",self.navigationItem);
-  
-  
-  UIButton *button = [[UIButton alloc] init];
-  
-  [button setImage:[UIImage imageNamed:@"menuButton"] forState:UIControlStateNormal];
-  [button setBackgroundColor:[UIColor blackColor]];
-  [button addTarget:self action:@selector(leftButtonPressed)
-   forControlEvents:UIControlEventTouchUpInside];
-  [button setFrame:CGRectMake(0, 0, 30, 30)];
-  
-  UIBarButtonItem *barButtonItem = [[UIBarButtonItem alloc]
-									initWithCustomView:button];
-  viewController.navigationItem.leftBarButtonItem = barButtonItem;
-  NSLog(@"left button = %@",self.navigationItem.leftBarButtonItem);
-}
-- (void)configureMenuButton {
-  NSMutableArray *leftBtns = [[NSMutableArray alloc] init];
-  
-  UIBarButtonItem *leftBtn = [[UIBarButtonItem alloc] initWithImage:[UIImage imageNamed:@"menuButton"] style:UIBarButtonItemStylePlain target:self action:@selector(leftButtonPressed)];
-  [leftBtns addObject:leftBtn];
-
-  NSLog(@"nav item = %@",self.navigationItem);
-  
-  
-  UIButton *button = [[UIButton alloc] init];
-  
-  [button setImage:[UIImage imageNamed:@"menuButton"] forState:UIControlStateNormal];
-  [button setBackgroundColor:[UIColor blackColor]];
-  [button addTarget:self action:@selector(leftButtonPressed)
-   forControlEvents:UIControlEventTouchUpInside];
-  [button setFrame:CGRectMake(0, 0, 30, 30)];
-  
-  UIBarButtonItem *barButtonItem = [[UIBarButtonItem alloc]
-									initWithCustomView:button];
-  self.navigationItem.leftBarButtonItem = barButtonItem;
-  NSLog(@"left button = %@",self.navigationItem.leftBarButtonItem);
-//  [self.navigationItem setLeftBarButtonItems:leftBtns animated:NO];
+  viewController.navigationItem.leftBarButtonItem = leftBtn;
 }
 
 -(void)leftButtonPressed {
-  NSLog(@"Left Button Tapped");
-  NSLog(@"self.delegate = %@",self.delegate);
   if ([self.slidingDelegate respondsToSelector:@selector(toggleLeftMenu)]) {
 	[self.slidingDelegate toggleLeftMenu];
   }
