5340603767a7a355a9cdb88f082b48f7755eec3c
hsusmita <susmita.horrow@gmail.com>
2015-10-28 13:53:31 +0530

    Reachability pod added

---
+++ ICSAdmin/ICSAdmin/API/APIError.h
@@ -0,0 +1,13 @@
+//
+//  APIError.h
+//  ICSAdmin
+//
+//  Created by Susmita Horrow on 28/10/15.
+//  Copyright © 2015 GHCI-CSPR. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface APIError : NSObject
+
+@end
